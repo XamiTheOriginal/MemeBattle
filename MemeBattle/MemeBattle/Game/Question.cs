7b5fbe9103ab4590c207dc2e968b247a8a49271f
XamiTheOriginal <122601380+XamiTheOriginal@users.noreply.github.com>
2025-03-17 09:27:08 +0530

    updated Question.cs

--- MemeBattle/MemeBattle/Game/Question.cs
+++ MemeBattle/MemeBattle/Game/Question.cs
@@ -2,5 +2,16 @@
 
 public class Question
 {
+    public string Text { get; set; }
+    public List<string> Choices { get; set; }
+    public int CorrectAnswer { get; set; }
     
+    public Question(string text, List<string> choices, int correctAnswer)
+    {
+        this.Text = text;
+        this.Choices = choices;
+        this.CorrectAnswer = correctAnswer;
+    }
+    
+    public bool IsCorrect(int answer) => answer == CorrectAnswer;
 }
